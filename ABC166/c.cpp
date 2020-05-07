585c70c8de412709b35d814c6978196ee7586407
yamatonao <yamatonao.net@gmail.com>
2020-05-07 23:03:29 +0900

    try ABC 166 and solved WA

---
+++ ABC166/c.cpp
@@ -0,0 +1,143 @@
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+// #include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define REP(i,n) for(ll i=0; i<(n); i++)
+#define REPR(i, n) for(ll i = n;i >= 0;i--)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+#define ALL(n) begin(n),end(n)
+#define IN(a, x, b) (a<=x && x<b)
+#define INIT std::ios::sync_with_stdio(false);std::cin.tie(0);
+template<class T> inline T CHMAX(T & a, const T b) { return a = (a < b) ? b : a; }
+template<class T> inline T CHMIN(T & a, const T b) { return a = (a > b) ? b : a; }
+
+const long double EPS = 1e-10;
+const long long INF = 1e18;
+const long double PI = acos(-1.0L);
+const long long MOD = 1000000007;
+
+ll tall[100010];
+bool ans[100010];
+
+int main(void){
+    ll n;
+    ll m;
+    cin >> n >> m;
+    REP(i,n){
+        ll tmp;
+        cin >> tmp;
+        tall[i+1] = tmp;
+    }
+
+    REP(i,m){
+        ll tmp1,tmp2;
+        cin >> tmp1 >> tmp2;
+        if(tall[tmp1] == tall[tmp2]){
+            ans[tmp1] = ans[tmp2] = true;
+        }else if(tall[tmp1] > tall[tmp2]){
+            ans[tmp2] = true;
+        }else if(tall[tmp1] < tall[tmp2]){
+            ans[tmp1] = true;
+        }
+    }
+
+    ll ansnow=0;
+
+    for(ll i=1; i<=n; i++){
+        if(ans[i] == false){
+            ansnow++;
+        }
+    }
+
+    cout << ansnow << endl;
+
+    return 0;
+}
