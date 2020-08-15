2bfb2c5afe40936fab2a07731cde2da4cd38bb98
yamatonao <yamatonao.net@gmail.com>
2020-08-15 18:27:08 +0900

    solve ABC174

---
+++ ABC174/e.cpp
@@ -0,0 +1,147 @@
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+// #include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef long double ld;
+#define REP(i,n) for(ll i=0; i<(n); i++)
+#define REPR(i, n) for(ll i = n;i >= 0;i--)
+#define FOR(i, m, n) for(ll i = m;i < n;i++)
+#define ALL(n) begin(n),end(n)
+#define IN(a, x, b) (a<=x && x<b)
+#define INIT std::ios::sync_with_stdio(false);std::cin.tie(0);
+template<class T> inline T CHMAX(T & a, const T b) { return a = (a < b) ? b : a; }
+template<class T> inline T CHMIN(T & a, const T b) { return a = (a > b) ? b : a; }
+
+const long double EPS = 1e-10;
+const long long INF = 1e18;
+const long double PI = acos(-1.0L);
+const long long MOD = 1000000007;
+
+vector<ll> input;
+
+int main(void){
+    ll n;
+    ll k;
+    ll max_length =0;
+    cin >> n;
+    cin >> k;
+    REP(i,n){
+        ll tmp;
+        cin >> tmp;
+        input.push_back(tmp);
+        CHMAX(max_length, tmp);
+    }
+                            // ↓普段は。今回はありえないので大丈夫。
+    ll left = 0;            //「index = 0」が条件を満たすこともあるので、初期値は -1
+    ll right = max_length;  // 「index = a.size()-1」が条件を満たさないこともあるので、初期値は a.size()
+
+    /* どんな二分探索でもここの書き方を変えずにできる！ */
+    while (right - left > 1) {
+        bool is_ok=false;
+        ll mid = left + (right - left) / 2;
+        // 判定部分
+        ll cnt=0;
+        for(ll i=0;i<n;i++){
+        	if(input[i]%mid==0){
+        		cnt+=(input[i]/mid)-1;
+        	}
+        	else{
+        	cnt+=(input[i])/(mid);
+        	}
+        }
+        if(cnt<=k){is_ok =true;}
+        // 判定部分ここまで
+        if (is_ok){right = mid;}
+        else{left = mid;}
+    }
+    /* left は条件を満たさない(NG)最大の値、right は条件を満たす(OK)最小の値になっている */
+    cout << right << endl;
+    return 0;
+}
